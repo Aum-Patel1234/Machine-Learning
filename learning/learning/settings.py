c66aa9e1f301360f823eb493bd4cc7c969d347b0
Aum Patel <aumpatel131@gmail.com>
2025-01-15 16:24:14 +0530

    added tailwind configurations....

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -38,8 +38,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # add all the new apps made
     'new_app',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS=['127.0.0.1']
+
+NPM_BIN_PATH = '/usr/bin/npm'  # didnt affect anything but still 
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -49,6 +58,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "learning.urls"
--- learning/learning/urls.py
+++ learning/learning/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('contact/',views.contact, name='contact'),
     path('new_app/',include('new_app.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
