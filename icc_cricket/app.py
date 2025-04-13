88306bc9cd653ec8e73245cb4f6ee015bf88dcfd
Aum Patel <aumpatel131@gmail.com>
2025-04-13 22:29:26 +0530

    update something

--- icc_cricket/app.py
+++ icc_cricket/app.py
@@ -29,4 +29,4 @@ def predict():
         return render_template('index.html', prediction=round(prediction, 2))
         
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
